ec079eb260ca42e6ec0a7b658fe9bcbf40ffdcec
Trang <nquynhtrang2402@gmail.com>
2025-11-04 22:38:35 +0700

    Ready for deployment to Platform.sh

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+"""Defines URL patterns for accounts app."""
+
+from django.urls import path, include
+
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+    # Include default auth urls.
+    path('', include('django.contrib.auth.urls')),
+    # Register page.
+    path('register/', views.register, name='register'),
+]
